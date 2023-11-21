8b69df7f77afd733de8f4cd2c79b6f056b391841
oussemasgh <oussema.sghaier@etudiant-enit.utm.tn>
2023-11-21 10:01:58 +0100

    project generation

---
+++ src/main/java/com/example/elastic11_21_2023/Elastic11212023Application.java
@@ -0,0 +1,13 @@
+package com.example.elastic11_21_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elastic11212023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Elastic11212023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elastic11_21_2023/Elastic11212023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elastic11_21_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Elastic11212023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
